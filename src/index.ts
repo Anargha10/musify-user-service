e04039ac3429c984165e97b7a47d196d0effeafe
Anargha Bhattacharjee <anarghabhatta369@gmail.com>
2025-05-27 20:26:03 +0530

    cors fix 2

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const connectDb= async()=>{
 }
 
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
 app.use('/api/v1', userRoutes);
 
 app.get('/', (req, res) => {
@@ -27,20 +27,20 @@ app.get('/', (req, res) => {
 });
 
 const allowedOrigins = ['https://www.imanargha.shop', 'https://api.imanargha.shop']; // Add your API domain too if the API itself might need to access something
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure OPTIONS is included
-    allowedHeaders: ['Content-Type', 'Authorization'], // Add any custom headers your frontend sends
-    credentials: true // If you are sending cookies/authentication headers
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // allow requests with no origin (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Ensure OPTIONS is included
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Add any custom headers your frontend sends
+//     credentials: true // If you are sending cookies/authentication headers
+// }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
