fdc2c2e848eb4f7f055102da88b162cd32b97dbe
Anargha Bhattacharjee <anarghabhatta369@gmail.com>
2025-05-31 22:31:09 +0530

    user cors

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ import cors from 'cors'
 
 dotenv.config();
 const app = express();
+app.use(cors({
+    origin: 'https://www.imanargha.shop', // Or specify '' if needed
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
+
 const PORT = process.env.PORT || 5000;
 
 const connectDb= async()=>{
